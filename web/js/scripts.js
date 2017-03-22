f02148f2ba41ff4e5a5ebec868d9e518b692f804
Adrián <alouroalonso_95@hotmail.com>
2017-03-22 01:30:09 +0000

    Modal box casi terminado
    Falta centrarlo.

---
+++ web/js/scripts.js
@@ -0,0 +1,11 @@
+// CURSOS
+function openNewClassDialog() {
+    $("#myModal").show();
+}
+
+// When the user clicks on <span> (x), close the modal
+function closeModal() {
+    $("#myModal").hide();
+}
+
+
